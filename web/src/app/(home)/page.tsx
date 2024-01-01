652e979f4a2d1dca67affbaee2d1ee7b25dafb46
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-12-31 22:58:19 -0300

    feat: initial commit - login interface

---
+++ web/src/app/(home)/page.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import Image from "next/image";
+import Logo from "../Logo.svg";
+import Google from "../Google.svg";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Separator } from "@/components/ui/separator";
+import { LockKeyhole, Mail } from "lucide-react";
+
+export default function Home() {
+  const { toast } = useToast();
+
+  return (
+    <div className="w-full block md:flex">
+      <div className="flex flex-col md:w-1/2 min-h-screen p-8 2xl:px-20">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={Logo}
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="object-cover w-10 h-10"
+          />
+          <div className="hidden md:block">
+            <h1 className="text-3xl font-bold gradient-logo">SpaceJam</h1>
+          </div>
+        </div>
+
+        <div className="flex-1 flex items-center w-full 2xl:px-36 xl:px-24 lg:px-14 md:px-10">
+          <div className="flex flex-col gap-8 w-full">
+            <div className="flex flex-col gap-4">
+              <h1 className="text-3xl">
+                Se jogue e dê o{" "}
+                <span className="gradient-text font-bold">play!</span>
+              </h1>
+              <h2 className="text-sm text-zinc-400 font-medium">
+                Faça login agora no SpaceJam e aproveite o melhor da música.
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="relative w-full">
+                <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-zinc-400 pointer-events-none">
+                  <Mail size={14} />
+                </div>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  className="pl-9 w-full transition-all focus-visible:ring-[#b7f09c8c] focus:border-[#82dbf7] focus-visible:ring-offset-1 focus-visible:ring-2"
+                />
+              </div>
+              <div className="relative w-full">
+                <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-zinc-400 pointer-events-none">
+                  <LockKeyhole size={14} />
+                </div>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  className="pl-9 w-full transition-all focus-visible:ring-[#b7f09c8c] focus:border-[#82dbf7] focus-visible:ring-offset-1 focus-visible:ring-2"
+                />
+              </div>
+
+              <div className="flex w-full justify-between mt-4">
+                <div className="flex gap-2">
+                  <Checkbox
+                    id="terms"
+                    className="border-accent data-[state=checked]:bg-[#b6f09c] data-[state=checked]:text-zinc-900 transition-all"
+                  />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm text-zinc-400 pt-[.1rem] leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <p
+                  className="gradient-text2 cursor-pointer text-sm font-semibold"
+                  onClick={() => {
+                    toast({
+                      title: "Ação Inválida",
+                      description: "Esta ação não está disponível no momento.",
+                    });
+                  }}
+                >
+                  Forgot Password?
+                </p>
+              </div>
+
+              <div className="flex w-full justify-between mt-4">
+                <Button
+                  className="w-full text-zinc-900 font-bold bg-[#B6F09C] hover:bg-[#9bf073] transition-all"
+                  onClick={() => {
+                    toast({
+                      title: "Ação Inválida",
+                      description: "Esta ação não está disponível no momento.",
+                    });
+                  }}
+                >
+                  Log in
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex gap-2 justify-center items-center">
+              <Separator className="w-[30%]" />
+              <p className="text-xs text-zinc-600 font-medium">
+                or continue with
+              </p>
+              <Separator className="w-[30%]" />
+            </div>
+
+            <Button className="w-full flex gap-2 text-zinc-500 font-bold bg-[#F0F0F0] bg-opacity-5 transition-all hover:bg-opacity-10 hover:text-zinc-400 hover:bg-[#F0F0F0]">
+              <Image
+                src={Google}
+                alt="logo"
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="object-cover w-5 h-5"
+              />
+              Google Account
+            </Button>
+          </div>
+        </div>
+
+        <p className="text-sm text-zinc-500 font-semibold">
+          {"Don't have an account?"}{" "}
+          <span
+            className="gradient-text3 cursor-pointer"
+            onClick={() => {
+              toast({
+                title: "Ação Inválida",
+                description: "Esta ação não está disponível no momento.",
+              });
+            }}
+          >
+            Sign Up
+          </span>
+        </p>
+      </div>
+      <div className="md:w-1/2 w-0 hidden md:block">
+        <Image
+          src="/abstract-01.png"
+          alt="logo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="object-cover w-full h-screen"
+        />
+      </div>
+    </div>
+  );
+}
